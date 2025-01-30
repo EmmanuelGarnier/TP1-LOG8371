bd0b0fc9148e0136860029fb8984851fc32d78b1
Elouan Guyon <elouan.guyon@polymtl.ca>
2025-01-30 18:17:31 -0500

    addednew feature and test

---
+++ server/sonar-main/src/test/java/org/sonar/application/AdditionTest.java
@@ -0,0 +1,45 @@
+/*
+ * SonarQube
+ * Copyright (C) 2009-2024 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.application;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AdditionTest {
+
+    @Test
+    public void testAddition() {
+        int result = Addition.addNumbers(5, 10);
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testNegativeNumberAdditiion() {
+        int result = Addition.addNumbers(-23, -9);
+        assertEquals(-32, result);
+    }
+
+
+    @Test
+    public void testResultZero() {
+        int result = Addition.addNumbers(-4, 4);
+        assertEquals(0, result);
+    }
+}
