5bd918b960a8fb7d3e83693b0397bd819e9130fe
EmmanuelGarnier <90262732+EmmanuelGarnier@users.noreply.github.com>
2025-02-10 04:20:16 -0500

    TEST 2 LIMA VIDEO

--- server/sonar-main/src/main/java/org/sonar/application/Addition.java
+++ server/sonar-main/src/main/java/org/sonar/application/Addition.java
@@ -17,14 +17,10 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-
-*
 package org.sonar.application;
 
-***
 public class Addition {
-    *
-   public static int addNumbers(int num1, int num2 ) {
- * return num1 + num2;
- * }*
+    public static int addNumbers(int num1, int num2) {
+        return num1 + num2;
+    }
 }
