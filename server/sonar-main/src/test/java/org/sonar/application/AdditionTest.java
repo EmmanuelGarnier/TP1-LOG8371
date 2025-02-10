75887f00580c9081236ac81c3ff6bac57600e397
EmmanuelGarnier <90262732+EmmanuelGarnier@users.noreply.github.com>
2025-02-10 03:38:48 -0500

    test part 2 video CI/CD

--- server/sonar-main/src/test/java/org/sonar/application/AdditionTest.java
+++ server/sonar-main/src/test/java/org/sonar/application/AdditionTest.java
@@ -10,36 +10,38 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package org.sonar.application;
-
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-public class AdditionTest {
-
-    @Test
-    public void testAddition() {
-        int result = Addition.addNumbers(5, 10);
-        assertEquals(15, result);
-    }
-
-    @Test
-    public void testNegativeNumberAdditiion() {
-        int result = Addition.addNumbers(-23, -9);
-        assertEquals(-32, result);
-    }
-
-
-    @Test
-    public void testResultZero() {
-        int result = Addition.addNumbers(-4, 4);
-        assertEquals(0, result);
-    }
-}
+/*
+ * package org.sonar.application;
+ * 
+ * import org.junit.Test;
+ * import static org.junit.Assert.*;
+ * 
+ * public class AdditionTest {
+ * 
+ * @Test
+ * public void testAddition() {
+ * int result = Addition.addNumbers(5, 10);
+ * assertEquals(15, result);
+ * }
+ * 
+ * @Test
+ * public void testNegativeNumberAdditiion() {
+ * int result = Addition.addNumbers(-23, -9);
+ * assertEquals(-32, result);
+ * }
+ * 
+ * 
+ * @Test
+ * public void testResultZero() {
+ * int result = Addition.addNumbers(-4, 4);
+ * assertEquals(0, result);
+ * }
+ * }
+ */
