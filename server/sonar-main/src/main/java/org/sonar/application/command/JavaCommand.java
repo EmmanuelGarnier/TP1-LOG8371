ae93521bde1fcd8783f1f5b59e7ecc625a1a48f8
yessirski <90262732+EmmanuelGarnier@users.noreply.github.com>
2025-02-10 22:08:59 -0500

    Update JavaCommand.java

--- server/sonar-main/src/main/java/org/sonar/application/command/JavaCommand.java
+++ server/sonar-main/src/main/java/org/sonar/application/command/JavaCommand.java
@@ -33,25 +33,43 @@ import org.sonar.process.System2;
 import static java.util.Objects.requireNonNull;
 
 public class JavaCommand<T extends JvmOptions> extends AbstractCommand<JavaCommand<T>> {
-  // program arguments
+  // Program arguments
   private final Map<String, String> arguments = new LinkedHashMap<>();
   private final List<String> parameters = new LinkedList<>();
-  // entry point
+  // Entry point
   private String className;
   private JvmOptions<T> jvmOptions;
-  // relative path to JAR files
+  // Relative path to JAR files
   private final List<String> classpath = new ArrayList<>();
   private boolean readsArgumentsFromFile;
   private Long gracefulStopTimeoutMs;
 
+  /**
+   * Constructs a new JavaCommand.
+   *
+   * @param id the process ID
+   * @param workDir the working directory
+   */
   public JavaCommand(ProcessId id, File workDir) {
     super(id, workDir, System2.INSTANCE);
   }
 
+  /**
+   * Returns the program arguments.
+   *
+   * @return the program arguments
+   */
   public Map<String, String> getArguments() {
     return arguments;
   }
 
+  /**
+   * Sets a program argument.
+   *
+   * @param key the argument key
+   * @param value the argument value
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> setArgument(String key, @Nullable String value) {
     if (value == null) {
       arguments.remove(key);
@@ -61,10 +79,21 @@ public class JavaCommand<T extends JvmOptions> extends AbstractCommand<JavaComma
     return this;
   }
 
+  /**
+   * Returns the parameters.
+   *
+   * @return the parameters
+   */
   public List<String> getParameters() {
     return parameters;
   }
 
+  /**
+   * Adds a parameter.
+   *
+   * @param parameter the parameter to add
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> addParameter(@Nullable String parameter) {
     if (parameter == null) {
       parameters.remove(parameter);
@@ -74,43 +103,92 @@ public class JavaCommand<T extends JvmOptions> extends AbstractCommand<JavaComma
     return this;
   }
 
+  /**
+   * Returns the class name.
+   *
+   * @return the class name
+   */
   public String getClassName() {
     return className;
   }
 
+  /**
+   * Sets the class name.
+   *
+   * @param className the class name to set
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> setClassName(String className) {
     this.className = className;
     return this;
   }
 
+  /**
+   * Returns the JVM options.
+   *
+   * @return the JVM options
+   */
   public JvmOptions<T> getJvmOptions() {
     return jvmOptions;
   }
 
+  /**
+   * Sets the JVM options.
+   *
+   * @param jvmOptions the JVM options to set
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> setJvmOptions(JvmOptions<T> jvmOptions) {
     this.jvmOptions = jvmOptions;
-
     return this;
   }
 
+  /**
+   * Returns the classpath.
+   *
+   * @return the classpath
+   */
   public List<String> getClasspath() {
     return classpath;
   }
 
+  /**
+   * Adds a classpath entry.
+   *
+   * @param s the classpath entry to add
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> addClasspath(String s) {
     classpath.add(s);
     return this;
   }
 
+  /**
+   * Returns whether arguments are read from a file.
+   *
+   * @return true if arguments are read from a file, false otherwise
+   */
   public boolean getReadsArgumentsFromFile() {
     return readsArgumentsFromFile;
   }
 
+  /**
+   * Sets whether arguments are read from a file.
+   *
+   * @param readsArgumentsFromFile true if arguments are read from a file, false otherwise
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> setReadsArgumentsFromFile(boolean readsArgumentsFromFile) {
     this.readsArgumentsFromFile = readsArgumentsFromFile;
     return this;
   }
 
+  /**
+   * Sets multiple arguments.
+   *
+   * @param args the arguments to set
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> setArguments(Properties args) {
     for (Map.Entry<Object, Object> entry : args.entrySet()) {
       setArgument(entry.getKey().toString(), entry.getValue() != null ? entry.getValue().toString() : null);
@@ -118,11 +196,22 @@ public class JavaCommand<T extends JvmOptions> extends AbstractCommand<JavaComma
     return this;
   }
 
+  /**
+   * Returns the graceful stop timeout in milliseconds.
+   *
+   * @return the graceful stop timeout in milliseconds
+   */
   public long getGracefulStopTimeoutMs() {
     requireNonNull(gracefulStopTimeoutMs, "gracefulStopTimeoutMs has not been set");
     return gracefulStopTimeoutMs;
   }
 
+  /**
+   * Sets the graceful stop timeout in milliseconds.
+   *
+   * @param gracefulStopTimeoutMs the graceful stop timeout in milliseconds
+   * @return the JavaCommand instance
+   */
   public JavaCommand<T> setGracefulStopTimeoutMs(long gracefulStopTimeoutMs) {
     this.gracefulStopTimeoutMs = gracefulStopTimeoutMs;
     return this;
